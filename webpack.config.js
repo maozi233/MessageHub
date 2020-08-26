5af7c3850444df0585819bfc393d674bb1084d8c
yaowei04361 <yaowei04361@hellobike.com>
2020-08-26 11:15:41 +0800

    feat(src): 添加本地服务方便调试

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: "./src/index.ts",
+  entry: "./src/index.ts", // 打包入口
   module: {
     rules: [
       {
@@ -11,7 +12,7 @@ module.exports = {
       }
     ],
   },
-  output: {
+  output: { // 打包出口
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist/umd'),
     globalObject: 'this', // 兼容node环境下的window问题
@@ -21,5 +22,22 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.ts'],
-  }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist/umd'),
+    port: 8766,
+    host: 'localhost',
+    compress: true,
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: { // 压缩HTML文件
+        removeComments: true, // 移除HTML中的注释
+        collapseWhitespace: false, // 删除空白符与换行符
+        minifyCSS: true// 压缩内联css
+      },
+    }),
+  ],
 };
