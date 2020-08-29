607b1e811872adbe365dec7df6be273bc4c6d8ed
yaowei04361 <yaowei04361@hellobike.com>
2020-08-29 12:39:32 +0800

    feat(types): 枚举改动

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,12 +1,24 @@
-import { TaskItemStatus } from './const';
-
 export interface HubConfig {
   timeout: number;
   log: boolean;
+}
+
+export enum TaskItemStatus {
+  /** 请求中 */
+  Pending = '请求中',
+  /** 请求完毕待执行 */
+  Resolved = '请求完毕待执行',
+  /** 请求失败 */
+  Rejected = '请求失败',
+  /** 已取消 */
+  Canceled = '已取消',
+  /** 已执行 */
+  Done = '已执行',
 }
 
 export interface Task {
   name: string;
   priority: number;
   status?: TaskItemStatus;
+  callback?: Function;
 }
--- src/types/type.ts
+++ src/types/type.ts
@@ -2,5 +2,5 @@ import { HubConfig } from './interface';
 
 export const defaultConfig: HubConfig = {
   timeout: 0,
-  log: false
-}
+  log: false,
+};
